852b0838d061c54a9e9a844094002e4785ad2106
Abdualla Ibrahim Al-Gouda <abduallah.ibrahim.gouda@gmail.com>
2021-01-30 03:04:12 +0200

    first structure To do list

--- app.js
+++ app.js
@@ -3,21 +3,40 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("view-engine","ejs");
+let items = [];
 
-app.get('/', function(req,res){
-    var today = new Date();
-    var options = {
-        weekday:"long" , 
-        day : "numeric" , 
-        month : "long"
-    }
-    var day = today.toLocaleString("en-US",options);
-    res.render("list.ejs",{day: day})
+app.set("view-engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+ 
+app.get('/', function (req, res) {
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    let day = today.toLocaleString("en-US", options);
+    res.render("list.ejs", {
+        day: day,
+        newlistitem: items
+    });
 })
 
 
-const port = 3000 ;
-app.listen(port , function (req,res){
+app.post('/', function (req, res) {
+    let item = req.body.newitem;
+    if (item === "") {
+        res.redirect("/");
+    }else {
+           items.push(item);
+    console.log(item);
+    res.redirect("/");
+        }
+ 
+})
+
+let port = 3000;
+app.listen(port, function (req, res) {
     console.log("server is up .. ")
 })
